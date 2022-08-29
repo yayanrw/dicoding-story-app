f7e56fc36dc87f0119c784b6b4506bd5e680d8f0
yayanrw <yayanraw@gmail.com>
2022-08-30 06:01:13 +0700

    save prefs

--- app/src/main/java/com/heyproject/storyapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/heyproject/storyapp/ui/login/LoginFragment.kt
@@ -7,13 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.heyproject.storyapp.R
 import com.heyproject.storyapp.databinding.FragmentLoginBinding
+import com.heyproject.storyapp.model.User
+import com.heyproject.storyapp.util.RequestState
+import com.heyproject.storyapp.util.UserPreference
 
 class LoginFragment : Fragment() {
 
     private var binding: FragmentLoginBinding? = null
     private val viewModel: LoginViewModel by viewModels()
+    private lateinit var user: User
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +36,26 @@ class LoginFragment : Fragment() {
             viewModel = viewModel
             loginFragment = this@LoginFragment
         }
+
+        viewModel.message.observe(viewLifecycleOwner) {
+            Snackbar.make(view, it!!, Snackbar.LENGTH_SHORT).show()
+        }
+
+        viewModel.requestState.observe(viewLifecycleOwner) {
+            if (it == RequestState.SUCCESS) {
+                findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            }
+        }
+
+        viewModel.loginResult.observe(viewLifecycleOwner) {
+            val userPreference = UserPreference(requireContext())
+
+            user.userId = it.userId
+            user.name = it.name
+            user.token = it.token
+
+            userPreference.setUser(user)
+        }
     }
 
     override fun onDestroyView() {
@@ -47,6 +72,5 @@ class LoginFragment : Fragment() {
             binding!!.edLoginEmail.text.toString(),
             binding!!.edLoginPassword.text.toString()
         )
-//        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
     }
 }
--- app/src/main/java/com/heyproject/storyapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/heyproject/storyapp/ui/login/LoginViewModel.kt
@@ -6,8 +6,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.heyproject.storyapp.network.StoryApi
+import com.heyproject.storyapp.network.response.LoginResult
 import com.heyproject.storyapp.util.RequestState
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 const val TAG = "LoginViewModel"
 
@@ -18,6 +21,9 @@ class LoginViewModel : ViewModel() {
     private val _message = MutableLiveData<String>()
     val message: LiveData<String> = _message
 
+    private val _loginResult = MutableLiveData<LoginResult>()
+    val loginResult: LiveData<LoginResult> = _loginResult
+
     fun signIn(
         email: String,
         password: String
@@ -26,10 +32,22 @@ class LoginViewModel : ViewModel() {
             try {
                 _requestState.value = RequestState.LOADING
                 val response = StoryApi.retrofitService.postLogin(email, password)
-                _requestState.value = RequestState.SUCCESS
-            } catch (e: Exception) {
+                if (!response.error!!) {
+                    _requestState.value = RequestState.SUCCESS
+                    _loginResult.value = response.loginResult!!
+                    _message.value = response.message ?: "Login successfully"
+                } else {
+                    _requestState.value = RequestState.ERROR
+                    _message.value = response.message ?: "Oops, something went wrong!"
+                }
+
+            } catch (e: HttpException) {
                 _requestState.value = RequestState.ERROR
-                _message.value = e.toString()
+                _message.value = "Oops, something went wrong!"
+                Log.e(TAG, e.toString())
+            } catch (e: IOException) {
+                _requestState.value = RequestState.ERROR
+                _message.value = "Couldn't reach server, check your internet connection."
                 Log.e(com.heyproject.storyapp.ui.register.TAG, e.toString())
             }
         }
