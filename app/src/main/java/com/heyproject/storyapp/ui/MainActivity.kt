8c9622487b56c8c56beb28378249c0684a37d0b8
yayanrw <yayanraw@gmail.com>
2022-08-29 12:15:55 +0700

    viewbinding and set navhost

--- app/src/main/java/com/heyproject/storyapp/ui/MainActivity.kt
+++ app/src/main/java/com/heyproject/storyapp/ui/MainActivity.kt
@@ -2,11 +2,29 @@ package com.heyproject.storyapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.heyproject.storyapp.R
+import com.heyproject.storyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
