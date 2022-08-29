ade93c8e5f861efb6f533a8795d57bf0b81f7d05
yayanrw <yayanraw@gmail.com>
2022-08-29 13:00:48 +0700

    register navigation

--- app/src/main/java/com/heyproject/storyapp/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/heyproject/storyapp/ui/register/RegisterFragment.kt
@@ -1,32 +1,38 @@
 package com.heyproject.storyapp.ui.register
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.heyproject.storyapp.R
+import com.heyproject.storyapp.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = RegisterFragment()
-    }
-
-    private lateinit var viewModel: RegisterViewModel
+    private var binding: FragmentRegisterBinding? = null
+    private val viewModel: RegisterViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        val registerBinding = FragmentRegisterBinding.inflate(inflater, container, false)
+        binding = registerBinding
+        return registerBinding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = viewModel
+            registerFragment = this@RegisterFragment
+        }
     }
 
+    fun register() {
+        findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+    }
 }
