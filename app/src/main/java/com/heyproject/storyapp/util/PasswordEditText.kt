640af465e99f191e77abee11b18d032324c75fcf
yayanrw <yayanraw@gmail.com>
2022-08-29 14:31:16 +0700

    add customview PasswordEditText.kt

---
+++ app/src/main/java/com/heyproject/storyapp/util/PasswordEditText.kt
@@ -0,0 +1,46 @@
+package com.heyproject.storyapp.util
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputEditText
+import com.heyproject.storyapp.R
+
+class PasswordEditText : TextInputEditText {
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                error = if (s!!.length < 7) {
+                    context.getString(R.string.minlength)
+                } else {
+                    null
+                }
+            }
+        })
+    }
+
+}
