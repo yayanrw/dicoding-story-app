daefccef8a5dd3bee5005451ae75006a55071644
yayanrw <yayanraw@gmail.com>
2022-08-29 15:20:47 +0700

    add response

---
+++ app/src/main/java/com/heyproject/storyapp/network/response/LoginResponse.kt
@@ -0,0 +1,27 @@
+package com.heyproject.storyapp.network.response
+
+import com.squareup.moshi.Json
+
+data class LoginResponse(
+
+    @Json(name = "loginResult")
+    val loginResult: LoginResult? = null,
+
+    @Json(name = "error")
+    val error: Boolean? = null,
+
+    @Json(name = "message")
+    val message: String? = null
+)
+
+data class LoginResult(
+
+    @Json(name = "name")
+    val name: String? = null,
+
+    @Json(name = "userId")
+    val userId: String? = null,
+
+    @Json(name = "token")
+    val token: String? = null
+)
