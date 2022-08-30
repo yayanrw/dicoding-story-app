8fa21ffe880c53d507f6c9929a2f7ebd17a76d9c
yayanrw <yayanraw@gmail.com>
2022-08-30 23:24:18 +0700

    add BindingAdapters.kt

---
+++ app/src/main/java/com/heyproject/storyapp/adapter/BindingAdapters.kt
@@ -0,0 +1,37 @@
+package com.heyproject.storyapp.adapter
+
+import android.view.View
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import coil.load
+import com.heyproject.storyapp.R
+import com.heyproject.storyapp.util.RequestState
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        imgView.load(imgUri) {
+            placeholder(R.drawable.loading_animation)
+            error(R.drawable.ic_broken_image)
+        }
+    }
+}
+
+@BindingAdapter("imageStatus")
+fun bindStatus(statusImageView: ImageView, status: RequestState?) {
+    when (status) {
+        RequestState.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        RequestState.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        else -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
+}
