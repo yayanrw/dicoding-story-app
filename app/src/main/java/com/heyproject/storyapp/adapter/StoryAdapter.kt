60c173ba9b092365ca8936abde2b271df8b8fa4b
yayanrw <yayanraw@gmail.com>
2022-08-30 23:24:44 +0700

    add StoryAdapter.kt

---
+++ app/src/main/java/com/heyproject/storyapp/adapter/StoryAdapter.kt
@@ -0,0 +1,29 @@
+package com.heyproject.storyapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.heyproject.storyapp.databinding.ItemStoryBinding
+import com.heyproject.storyapp.network.response.ListStoryItem
+
+class StoryAdapter(private var stories: List<ListStoryItem>) :
+    RecyclerView.Adapter<StoryAdapter.StoryViewHolder>() {
+    class StoryViewHolder(var binding: ItemStoryBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(listStoryItem: ListStoryItem) {
+            binding.tvName.text = listStoryItem.name
+            binding.tvDescription.text = listStoryItem.description
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryViewHolder {
+        val binding = ItemStoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return StoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: StoryViewHolder, position: Int) {
+        val story = stories[position]
+        holder.bind(story)
+    }
+
+    override fun getItemCount(): Int = stories.size
+}
