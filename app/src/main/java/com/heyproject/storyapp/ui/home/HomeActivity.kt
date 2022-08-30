7b52eb8d3566fb27326966a32954648329657fa8
yayanrw <yayanraw@gmail.com>
2022-08-30 18:22:50 +0700

    add option menu

--- app/src/main/java/com/heyproject/storyapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/heyproject/storyapp/ui/home/HomeActivity.kt
@@ -1,16 +1,21 @@
 package com.heyproject.storyapp.ui.home
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.heyproject.storyapp.R
 import com.heyproject.storyapp.databinding.ActivityHomeBinding
+import com.heyproject.storyapp.util.UserPreference
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var navController: NavController
+    private lateinit var userPreference: UserPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +26,31 @@ class HomeActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_home) as NavHostFragment
         navController = navHostFragment.navController
 
+        userPreference = UserPreference(this)
+
         setupActionBarWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return super.onSupportNavigateUp() || navController.navigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                userPreference.removeSharedPref()
+                navController.navigate(R.id.action_homeFragment_to_mainActivity)
+                true
+            }
+            else -> {
+                true
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.home_menu, menu)
+        return true
     }
 }
