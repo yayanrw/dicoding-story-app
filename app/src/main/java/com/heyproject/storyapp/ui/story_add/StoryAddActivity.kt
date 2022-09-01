02e9732c96e5ef47206caf653b30591dc944ffa2
yayanrw <yayanraw@gmail.com>
2022-09-02 01:17:38 +0700

    upload click

--- app/src/main/java/com/heyproject/storyapp/ui/story_add/StoryAddActivity.kt
+++ app/src/main/java/com/heyproject/storyapp/ui/story_add/StoryAddActivity.kt
@@ -12,18 +12,36 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
+import com.heyproject.storyapp.R
 import com.heyproject.storyapp.databinding.ActivityStoryAddBinding
+import com.heyproject.storyapp.network.StoryApi
+import com.heyproject.storyapp.util.UserPreference
+import com.heyproject.storyapp.util.reduceFileImage
 import com.heyproject.storyapp.util.rotateBitmap
 import com.heyproject.storyapp.util.uriToFile
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import retrofit2.HttpException
 import java.io.File
+import java.io.IOException
 
 class StoryAddActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStoryAddBinding
+    private var getFile: File? = null
+    private lateinit var userPreference: UserPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStoryAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        userPreference = UserPreference(this)
 
         if (!allPermissionsGranted()) {
             ActivityCompat.requestPermissions(
@@ -71,8 +89,9 @@ class StoryAddActivity : AppCompatActivity() {
         if (it.resultCode == CAMERA_X_RESULT) {
             val myFile = it.data?.getSerializableExtra("picture") as File
             val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+            getFile = myFile
             val result = rotateBitmap(
-                BitmapFactory.decodeFile(myFile.path),
+                BitmapFactory.decodeFile(getFile?.path),
                 isBackCamera
             )
 
@@ -86,6 +105,7 @@ class StoryAddActivity : AppCompatActivity() {
         if (result.resultCode == RESULT_OK) {
             val selectedImg: Uri = result.data?.data as Uri
             val myFile = uriToFile(selectedImg, this)
+            getFile = myFile
             binding.ivPreview.setImageURI(selectedImg)
         }
     }
@@ -96,6 +116,45 @@ class StoryAddActivity : AppCompatActivity() {
         intent.type = "image/*"
         val chooser = Intent.createChooser(intent, "Choose a Picture")
         launcherIntentGallery.launch(chooser)
+    }
+
+    fun uploadImage() {
+        if (getFile != null) {
+            val file = reduceFileImage(getFile as File)
+
+            val description =
+                binding.edAddDescription.text.toString().toRequestBody("text/plain".toMediaType())
+            val requestImageFile = file.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
+                "photo",
+                file.name,
+                requestImageFile
+            )
+
+            lifecycleScope.launch {
+                try {
+                    val response = StoryApi.retrofitService.insertStory(
+                        photo = imageMultipart,
+                        description = description,
+                        auth = "Bearer ${userPreference.getUser().token!!}"
+                    )
+                    Toast.makeText(this@StoryAddActivity, response.message, Toast.LENGTH_SHORT)
+                        .show()
+                } catch (e: HttpException) {
+                    Toast.makeText(this@StoryAddActivity, getString(R.string.oops), Toast.LENGTH_SHORT)
+                        .show()
+                } catch (e: IOException) {
+                    Toast.makeText(this@StoryAddActivity, getString(R.string.check_connection), Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        } else {
+            Toast.makeText(
+                this,
+                "Silakan masukkan berkas gambar terlebih dahulu.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     companion object {
