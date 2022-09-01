3517e0af91ef1d8ec86032b9ead5deb7fd11d198
yayanrw <yayanraw@gmail.com>
2022-09-01 23:07:48 +0700

    add story add

---
+++ app/src/main/java/com/heyproject/storyapp/ui/story_add/StoryAddActivity.kt
@@ -0,0 +1,12 @@
+package com.heyproject.storyapp.ui.story_add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.heyproject.storyapp.R
+
+class StoryAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_add)
+    }
+}
--- app/src/main/java/com/heyproject/storyapp/ui/story_add/StoryAddViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.heyproject.storyapp.ui.story_add
-
-import androidx.lifecycle.ViewModel
-
-class StoryAddViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
