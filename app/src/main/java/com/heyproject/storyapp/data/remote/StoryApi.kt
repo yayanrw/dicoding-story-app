06d76ad777fcf5a829a3c181781bca922655f2da
yayanrw <yayanraw@gmail.com>
2022-09-17 13:33:18 +0700

    init StoryApi.kt

--- app/src/main/java/com/heyproject/storyapp/data/remote/StoryApi.kt
+++ app/src/main/java/com/heyproject/storyapp/data/remote/StoryApi.kt
@@ -1,4 +1,41 @@
 package com.heyproject.storyapp.data.remote
 
-class StoryApi {
+import com.heyproject.storyapp.data.network.response.LoginResponse
+import com.heyproject.storyapp.data.network.response.StoriesResponse
+import com.heyproject.storyapp.data.remote.response.GeneralResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.*
+
+interface StoryApi {
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun postRegister(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): GeneralResponse
+
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun postLogin(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): LoginResponse
+
+    @Multipart
+    @POST("stories")
+    suspend fun postStory(
+        @Part photo: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+        @Header("Authorization") auth: String
+    ): GeneralResponse
+
+    @GET("stories")
+    suspend fun getStories(
+        @Query("page") page: Int,
+        @Query("size") size: Int,
+        @Query("location") location: Int,
+        @Header("Authorization") auth: String
+    ): StoriesResponse
 }
