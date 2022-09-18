c7c8f476122a54851136d82485fcc0dc90683ca7
yayanrw <yayanraw@gmail.com>
2022-09-18 15:18:43 +0700

    move to suspend post

--- app/src/main/java/com/heyproject/storyapp/domain/repository/StoryRepository.kt
+++ app/src/main/java/com/heyproject/storyapp/domain/repository/StoryRepository.kt
@@ -6,8 +6,6 @@ import com.heyproject.storyapp.data.remote.response.GeneralResponse
 import com.heyproject.storyapp.domain.PostStoryParams
 import com.heyproject.storyapp.domain.StoryParams
 import com.heyproject.storyapp.domain.UserModel
-import com.heyproject.storyapp.domain.model.General
-import com.heyproject.storyapp.domain.model.LoginResult
 import com.heyproject.storyapp.domain.model.Story
 import kotlinx.coroutines.flow.Flow
 
