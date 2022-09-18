dad623735df9bc5141771018f506c55c936ba879
yayanrw <yayanraw@gmail.com>
2022-09-18 11:09:53 +0700

    add StoryRepository.kt

---
+++ app/src/main/java/com/heyproject/storyapp/domain/repository/StoryRepository.kt
@@ -0,0 +1,17 @@
+package com.heyproject.storyapp.domain.repository
+
+import com.heyproject.storyapp.core.Resource
+import com.heyproject.storyapp.domain.PostStoryParams
+import com.heyproject.storyapp.domain.StoryParams
+import com.heyproject.storyapp.domain.UserModel
+import com.heyproject.storyapp.domain.model.General
+import com.heyproject.storyapp.domain.model.LoginResult
+import com.heyproject.storyapp.domain.model.Story
+import kotlinx.coroutines.flow.Flow
+
+interface StoryRepository {
+    fun getStories(storyParams: StoryParams): Flow<Resource<List<Story>>>
+    fun postRegister(user: UserModel): Flow<Resource<General>>
+    fun postLogin(user: UserModel): Flow<Resource<LoginResult>>
+    fun postStory(postStoryParams: PostStoryParams): Flow<Resource<General>>
+}
