9a395c376e8bb8fc3660e1379a1e630fdad7c34d
yayanrw <yayanraw@gmail.com>
2022-09-18 11:53:25 +0700

    LocalDataSource.kt fix

--- app/src/main/java/com/heyproject/storyapp/data/local/LocalDataSource.kt
+++ app/src/main/java/com/heyproject/storyapp/data/local/LocalDataSource.kt
@@ -1,4 +1,12 @@
 package com.heyproject.storyapp.data.local
 
-class LocalDataSource {
+import com.heyproject.storyapp.data.local.dao.StoryDao
+import com.heyproject.storyapp.data.local.entity.StoryEntity
+import kotlinx.coroutines.flow.Flow
+
+class LocalDataSource(
+    private val storyDao: StoryDao
+) {
+    fun getStories(): Flow<List<StoryEntity>> = storyDao.getStories()
+    suspend fun insertStories(stories: List<StoryEntity>) = storyDao.insertStory(stories)
 }
