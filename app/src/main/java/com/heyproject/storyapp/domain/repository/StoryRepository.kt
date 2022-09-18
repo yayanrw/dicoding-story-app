35d81a5d98bbca4b519276962c6a31635c755e7d
yayanrw <yayanraw@gmail.com>
2022-09-18 11:50:20 +0700

    StoryRepository.kt

--- app/src/main/java/com/heyproject/storyapp/domain/repository/StoryRepository.kt
+++ app/src/main/java/com/heyproject/storyapp/domain/repository/StoryRepository.kt
@@ -1,17 +1,17 @@
 package com.heyproject.storyapp.domain.repository
 
-import com.heyproject.storyapp.core.Resource
+import com.heyproject.storyapp.common.Resource
+import com.heyproject.storyapp.data.remote.dto.LoginResultDto
+import com.heyproject.storyapp.data.remote.dto.StoryDto
+import com.heyproject.storyapp.data.remote.response.GeneralResponse
 import com.heyproject.storyapp.domain.PostStoryParams
 import com.heyproject.storyapp.domain.StoryParams
 import com.heyproject.storyapp.domain.UserModel
-import com.heyproject.storyapp.domain.model.General
-import com.heyproject.storyapp.domain.model.LoginResult
-import com.heyproject.storyapp.domain.model.Story
 import kotlinx.coroutines.flow.Flow
 
 interface StoryRepository {
-    fun getStories(storyParams: StoryParams): Flow<Resource<List<Story>>>
-    fun postRegister(user: UserModel): Flow<Resource<General>>
-    fun postLogin(user: UserModel): Flow<Resource<LoginResult>>
-    fun postStory(postStoryParams: PostStoryParams): Flow<Resource<General>>
+    fun getStories(storyParams: StoryParams): Flow<Resource<List<StoryDto>>>
+    fun postRegister(user: UserModel): Flow<Resource<GeneralResponse>>
+    fun postLogin(user: UserModel): Flow<Resource<LoginResultDto>>
+    fun postStory(postStoryParams: PostStoryParams): Flow<Resource<GeneralResponse>>
 }
