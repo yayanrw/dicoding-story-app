1f7564c1c9114e2c5d0f0ca86215f785497a35bc
yayanrw <yayanraw@gmail.com>
2022-09-23 06:04:35 +0700

    fix UserPreferencesRepositoryImpl.kt

---
+++ app/src/main/java/com/heyproject/storyapp/domain/repository/UserPreferencesRepository.kt
@@ -0,0 +1,17 @@
+package com.heyproject.storyapp.domain.repository
+
+import com.heyproject.storyapp.base.arch.BaseContract
+import com.heyproject.storyapp.base.wrapper.DataResource
+import com.heyproject.storyapp.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
+/**
+Written by Yayan Rahmat Wijaya on 9/23/2022 04:14
+Github : https://github.com/yayanrw
+ **/
+
+interface UserPreferencesRepository : BaseContract.BaseRepository {
+    fun getUser(): Flow<DataResource<User>>
+    suspend fun saveUser(user: User): Flow<DataResource<Boolean>>
+    suspend fun removeUser(user: User): Flow<DataResource<Boolean>>
+}
