e727afa7959b21f10cdcc46e1d80f26b78f1e202
yayanrw <yayanraw@gmail.com>
2022-09-22 23:10:40 +0700

    model domain

--- app/src/main/java/com/heyproject/storyapp/domain/model/General.kt
+++ app/src/main/java/com/heyproject/storyapp/domain/model/General.kt
@@ -1,6 +1,11 @@
 package com.heyproject.storyapp.domain.model
 
+import com.heyproject.storyapp.data.datasource.remote.response.GeneralResponse
+
 data class General(
     val error: Boolean?,
     val message: String?
 )
+
+fun GeneralResponse?.toGeneral() =
+    General(error = this?.error ?: false, message = this?.message.orEmpty())
--- app/src/main/java/com/heyproject/storyapp/domain/model/LoginResult.kt
+++ app/src/main/java/com/heyproject/storyapp/domain/model/LoginResult.kt
@@ -1,7 +1,12 @@
 package com.heyproject.storyapp.domain.model
 
+import com.heyproject.storyapp.data.datasource.remote.dto.LoginResultDto
+
 data class LoginResult(
     val name: String?,
     val userId: String?,
     val token: String?
 )
+
+fun LoginResultDto.toLoginResult() =
+    LoginResult(name = this.name.orEmpty(), userId.orEmpty(), token.orEmpty())
