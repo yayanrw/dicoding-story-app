810ff4b8c452fe95b26b46a20f52ce1b2fe723da
yayanrw <yayanraw@gmail.com>
2022-09-23 10:34:14 +0700

    add PostRegisterUseCase.kt

---
+++ app/src/main/java/com/heyproject/storyapp/domain/usecase/PostRegisterUseCase.kt
@@ -0,0 +1,35 @@
+package com.heyproject.storyapp.domain.usecase
+
+import com.heyproject.storyapp.base.arch.BaseUseCase
+import com.heyproject.storyapp.base.wrapper.DataResource
+import com.heyproject.storyapp.base.wrapper.ViewResource
+import com.heyproject.storyapp.domain.model.General
+import com.heyproject.storyapp.domain.model.User
+import com.heyproject.storyapp.domain.model.toGeneral
+import com.heyproject.storyapp.domain.repository.StoryRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+/**
+Written by Yayan Rahmat Wijaya on 9/23/2022 09:43
+Github : https://github.com/yayanrw
+ **/
+
+class PostRegisterUseCase(
+    private val storyRepository: StoryRepository,
+    dispatcher: CoroutineDispatcher
+) : BaseUseCase<User, General>(dispatcher) {
+    override suspend fun execute(param: User?): Flow<ViewResource<General>> {
+        return storyRepository.postRegister(param!!).map { networkResult ->
+            when (networkResult) {
+                is DataResource.Success -> {
+                    ViewResource.Success(networkResult.data.toGeneral())
+                }
+                is DataResource.Error -> {
+                    ViewResource.Error(networkResult.exception)
+                }
+            }
+        }
+    }
+}
