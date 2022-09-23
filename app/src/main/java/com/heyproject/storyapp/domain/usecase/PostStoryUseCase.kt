58228dba02ee73eda6b0e24c1b619d17b322217f
yayanrw <yayanraw@gmail.com>
2022-09-23 10:38:50 +0700

    add PostStoryUseCase.kt on start

--- app/src/main/java/com/heyproject/storyapp/domain/usecase/PostStoryUseCase.kt
+++ app/src/main/java/com/heyproject/storyapp/domain/usecase/PostStoryUseCase.kt
@@ -10,6 +10,7 @@ import com.heyproject.storyapp.domain.repository.StoryRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 /**
 Written by Yayan Rahmat Wijaya on 9/23/2022 10:34
@@ -30,7 +31,7 @@ class PostStoryUseCase(
                     ViewResource.Error(networkResult.exception)
                 }
             }
-        }
+        }.onStart { emit(ViewResource.Loading()) }
     }
 
 }
