12f14525a63d1e7d45666a1eb58fc75c479ebb73
yayanrw <yayanraw@gmail.com>
2022-09-26 15:31:26 +0700

    disable vm

--- app/src/main/java/com/heyproject/storyapp/presentation/register/RegisterViewModel.kt
+++ app/src/main/java/com/heyproject/storyapp/presentation/register/RegisterViewModel.kt
@@ -1,44 +1,40 @@
 package com.heyproject.storyapp.presentation.register
 
-import androidx.lifecycle.*
-import com.heyproject.storyapp.data.network.StoryApi
-import kotlinx.coroutines.launch
-import retrofit2.HttpException
-import java.io.IOException
+import androidx.lifecycle.ViewModel
 
 
-class RegisterViewModel(private val pref: UserPreference) : ViewModel() {
-    private val _requestState = MutableLiveData<RequestState>()
-    val requestState: LiveData<RequestState> = _requestState
+class RegisterViewModel() : ViewModel() {
+//    private val _requestState = MutableLiveData<RequestState>()
+//    val requestState: LiveData<RequestState> = _requestState
 
-    fun register(name: String, email: String, password: String) {
-        viewModelScope.launch {
-            try {
-                _requestState.value = RequestState.LOADING
-                val response =
-                    StoryApi.retrofitService.postRegister(
-                        name,
-                        email,
-                        password
-                    )
-                if (!response.error!!) {
-                    _requestState.value = RequestState.SUCCESS
-                } else {
-                    _requestState.value = RequestState.ERROR
-                }
-            } catch (e: HttpException) {
-                if (e.code() == 400) {
-                    _requestState.value = RequestState.EMAIL_TAKEN
-                } else {
-                    _requestState.value = RequestState.ERROR
-                }
-            } catch (e: IOException) {
-                _requestState.value = RequestState.NO_CONNECTION
-            }
-        }
-    }
-
-    fun getUser(): LiveData<User> {
-        return pref.getUser().asLiveData()
-    }
+//    fun register(name: String, email: String, password: String) {
+//        viewModelScope.launch {
+//            try {
+//                _requestState.value = RequestState.LOADING
+//                val response =
+//                    StoryApi.retrofitService.postRegister(
+//                        name,
+//                        email,
+//                        password
+//                    )
+//                if (!response.error!!) {
+//                    _requestState.value = RequestState.SUCCESS
+//                } else {
+//                    _requestState.value = RequestState.ERROR
+//                }
+//            } catch (e: HttpException) {
+//                if (e.code() == 400) {
+//                    _requestState.value = RequestState.EMAIL_TAKEN
+//                } else {
+//                    _requestState.value = RequestState.ERROR
+//                }
+//            } catch (e: IOException) {
+//                _requestState.value = RequestState.NO_CONNECTION
+//            }
+//        }
+//    }
+//
+//    fun getUser(): LiveData<User> {
+//        return pref.getUser().asLiveData()
+//    }
 }
